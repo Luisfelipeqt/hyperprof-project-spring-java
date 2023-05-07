004e6e01cd8c2e91bc327008c0fc2d58ca0ce879
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-05-07 11:31:53 -0300

    Criando a rota de cadastro de aula

---
+++ src/main/java/br/com/treinaweb/hyperprof/api/alunos/controllers/AlunoController.java
@@ -0,0 +1,25 @@
+package br.com.treinaweb.hyperprof.api.alunos.controllers;
+
+import br.com.treinaweb.hyperprof.api.alunos.dtos.AlunoRequest;
+import br.com.treinaweb.hyperprof.api.alunos.dtos.AlunoResponse;
+import br.com.treinaweb.hyperprof.api.alunos.services.AlunoService;
+import br.com.treinaweb.hyperprof.api.common.routes.ApiRoutes;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = ApiRoutes.CADASTRAR_ALUNO)
+    public AlunoResponse cadastrarAluno
+            (@RequestBody @Valid AlunoRequest alunoRequest,
+             @PathVariable Long professorId) {
+        return alunoService.cadastrarAluno(alunoRequest, professorId);
+    }
+}
---
+++ src/main/java/br/com/treinaweb/hyperprof/api/alunos/services/AlunoServiceImpl.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.hyperprof.api.alunos.services;
+
+import br.com.treinaweb.hyperprof.api.alunos.dtos.AlunoRequest;
+import br.com.treinaweb.hyperprof.api.alunos.dtos.AlunoResponse;
+import br.com.treinaweb.hyperprof.api.alunos.mappers.AlunoMapper;
+import br.com.treinaweb.hyperprof.core.exceptions.ProfessorNotFoundException;
+import br.com.treinaweb.hyperprof.core.repositories.AlunoRepository;
+import br.com.treinaweb.hyperprof.core.repositories.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoServiceImpl implements AlunoService {
+
+    private final AlunoRepository alunoRepository;
+
+    private final AlunoMapper alunoMapper;
+
+    private final ProfessorRepository professorRepository;
+
+    @Override
+    public AlunoResponse cadastrarAluno(AlunoRequest alunoRequest, Long professorId) {
+        var professor = professorRepository.findById(professorId).orElseThrow(ProfessorNotFoundException::new);
+        var alunoParaCadastrar = alunoMapper.toAluno(alunoRequest);
+        alunoParaCadastrar.setProfessor(professor);
+        var alunoCadastrado = alunoRepository.save(alunoParaCadastrar);
+        return alunoMapper.toAlunoResponse(alunoCadastrado);
+    }
+}
